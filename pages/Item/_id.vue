edc5c0b7ec3d23f5412ba7e9dd9fdad8c9d1319e
steven <steven@giantzoc.com>
2019-09-22 14:32:40 -0500

    Updating look of item page

--- pages/Item/_id.vue
+++ pages/Item/_id.vue
@@ -1,6 +1,39 @@
 <template>
   <div v-if="Item">
-    <h3>{{ Item.name }}</h3>
+    <v-card>
+      <v-row>
+        <v-col md="auto">
+          <v-img
+            max-width="400"
+            :src="require(`~/static${Item.img}`)"
+            :srcset="require(`~/static${Item.img}`).srcSet"
+            :lazy-src="require(`~/static${Item.img}`).placeholder"
+          ></v-img>
+        </v-col>
+        <v-col>
+          <v-list>
+            <v-list-item>
+              <v-list-item-content>
+                <h3>{{Item.name}}</h3>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Description: {{Item.description}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Tier: {{Item.tier}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Type: {{Item.type}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content><div class="font-italic">{{Item.flavortext}}</div></v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-card-text></v-card-text>
+        </v-col>
+      </v-row>
+    </v-card>
   </div>
 </template>
 
@@ -11,14 +44,14 @@ export default {
     Item: {
       query: getItem,
       prefetch: ({ route }) => ({ itemId: route.params.id }),
-      variables () {
+      variables() {
         return { itemId: this.$route.params.id }
       }
     }
   },
-  head () {
+  head() {
     return {
-      title: (this.Item ? `${this.Item.name}` : 'Loading')
+      title: this.Item ? `${this.Item.name}` : 'Loading'
     }
   }
 }
