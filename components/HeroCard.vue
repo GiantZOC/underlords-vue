78ce7940b9e0160f39088d559e1f88ae5c0171de
steven <steven@giantzoc.com>
2019-09-30 15:32:48 -0500

    Added link to Hero Card

--- components/HeroCard.vue
+++ components/HeroCard.vue
@@ -1,15 +1,14 @@
 <template>
-
   <v-card>
-    <NuxtLink :to="`Hero/${_id}`">
-    <v-img
-      class="hero_image"
-      :src="require(`~/static${Hero_Image}`)"
-      :srcset="require(`~/static${Hero_Image}`).srcSet"
-      :lazy-src="require(`~/static${Hero_Image}`).placeholder"
-    >
-      <div class="title_blur">
-        <v-card-title class="hero_title align-end fill-height" style="text-shadow: 0 1px 0 black;">
+    <NuxtLink :to="`../Hero/${_id}`">
+      <v-img
+        class="hero_image"
+        :src="require(`~/static${Hero_Image}`)"
+        :srcset="require(`~/static${Hero_Image}`).srcSet"
+        :lazy-src="require(`~/static${Hero_Image}`).placeholder"
+      >
+        <div class="title_blur">
+          <v-card-title class="hero_title align-end fill-height" style="text-shadow: 0 1px 0 black;">
           <h3>{{Name}} <v-avatar v-for="alliance in Alliances" v-bind:key="alliance">
             <v-img
               :src="require(`~/static/images/alliances/${alliance.name.toLowerCase().replace(/\s/g, '')}.png`)"
@@ -18,8 +17,8 @@
             ></v-img>
           </v-avatar></h3>
         </v-card-title>
-      </div>
-    </v-img>
+        </div>
+      </v-img>
     </NuxtLink>
     <v-card v-for="Ability in Abilities" v-bind:key="Ability.Name" dark>
       <v-list-item three-line>
@@ -39,7 +38,6 @@
       </v-list-item>
     </v-card>
   </v-card>
-
 </template>
 
 
@@ -51,8 +49,8 @@ export default {
       type: String,
       required: true
     },
-    Alliances:{
-      name:{
+    Alliances: {
+      name: {
         type: String,
         required: true
       }
@@ -65,7 +63,7 @@ export default {
       type: String,
       required: true
     },
-    Abilities:{
+    Abilities: {
       Ability: {
         type: String,
         required: true
@@ -84,41 +82,40 @@ export default {
 </script>
 
 <style>
-
-  .title_blur {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    background: inherit;
-    background-attachment: fixed;
-    overflow: hidden;
-  }
-  .title_blur::before {
-    content: '';
-    position: absolute;
-    top: -20px;
-    left: 0;
-    width: 200%;
-    height: 200%;
-    background: inherit;
-    background-attachment: fixed;
-    -webkit-filter: blur(4px);
-    filter: blur(4px);
-  }
-  .title_blur::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-  }
-  .title_blur > .hero_title > h3 {
-    margin: 0;
-    color: white;
-    position: relative;
-    z-index: 1;
-  }
+.title_blur {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: inherit;
+  background-attachment: fixed;
+  overflow: hidden;
+}
+.title_blur::before {
+  content: '';
+  position: absolute;
+  top: -20px;
+  left: 0;
+  width: 200%;
+  height: 200%;
+  background: inherit;
+  background-attachment: fixed;
+  -webkit-filter: blur(4px);
+  filter: blur(4px);
+}
+.title_blur::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+}
+.title_blur > .hero_title > h3 {
+  margin: 0;
+  color: white;
+  position: relative;
+  z-index: 1;
+}
 </style>
