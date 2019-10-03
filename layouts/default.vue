6d6ca8b6794235745c352863e350d86093f80037
steven <steven@giantzoc.com>
2019-10-03 12:50:35 -0500

    Fixing navigation drawer on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
         :srcset="require(`~/static/images/background.jpg`).srcSet"
         :lazy-src="require(`~/static/images/background.jpg`).placeholder"
       >
-    <v-navigation-drawer class="visible-background" v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+    <v-navigation-drawer class="visible-background" v-model="drawer" :clipped="true" :absolute="true" fixed app>
       <v-list>
         <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
@@ -17,7 +17,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar class="visible-background" :clipped-left="clipped" app>
+    <v-app-bar class="visible-background" :clipped-left="true" app>
       <v-app-bar-nav-icon x-large @click.stop="drawer = !drawer" />
       <div class="container">
       <div class="d-flex justify-center align-center">
@@ -44,7 +44,7 @@
       
     </v-content>
 
-    <v-footer :fixed="fixed" app :absolute="true" inset>
+    <v-footer :fixed="false" app :absolute="true" inset>
       <span class="caption">
         Game content and materials are trademarks and copyrights of their respective publisher and its licensors. All rights reserved.
         This site is not affiliated with the game publisher.
@@ -58,9 +58,7 @@
 export default {
   data() {
     return {
-      clipped: true,
       drawer: true,
-      fixed: false,
       items: [
         {
           icon: 'mdi-apps',
@@ -84,13 +82,15 @@ export default {
         },
         {
           icon: 'mdi-dice-6',
-          title: 'Loot Rounds',
+          title: 'Loot Table',
           to: '/LootTable'
+        },
+        {
+          icon: 'mdi-dice-6',
+          title: 'Loot Rounds',
+          to: '/LootRounds'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: 'Underlords Stats'
     }
   }
